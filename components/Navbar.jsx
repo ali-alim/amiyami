04c65cc451fcf1472a78a3d10d26d5c0e7002bf0
ali-alim <ali.mamedov.2016@gmail.com>
2022-06-18 23:23:38 +0400

    build Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/../public/img/telephone.png" alt="" width="32" height="32" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>555 555 555</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <li className={styles.listItem}><em><strong style={{fontSize:'40px'}}>amiYami</strong></em></li>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+        <Image src="/../public/img/cart2.png" alt="cart" width="30" height="30" />
+        <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
